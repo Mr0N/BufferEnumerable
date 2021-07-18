45870800f365f4c0158b05cab65fb4c07e31b7f1
new_user <new_user@gmail.com>
2021-07-19 00:47:01 +0300

    =

--- Buffer/Program.cs
+++ Buffer/Program.cs
@@ -1,41 +1,24 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Ext
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Buffer");
-            var result = Enumerable.Range(0, 1)
-                .Select(a =>
-               {
-                   Thread.Sleep(10);
-                   return DateTime.Now.Millisecond;
-               }).ToBuffer();
-            for (int i = 0; i < 10; i++)
+            int count = 0;
+            var result = Enumerable.Range(0, 5).Select(a => count++).ToBuffer();
+            foreach (var item in result)
             {
-                foreach (var item in result)
-                {
-                    Console.WriteLine(item);
-                }
+                count++;
+                Console.WriteLine(item);
             }
-            Console.WriteLine("Not Buffer");
-            var resultX = Enumerable.Range(0, 1)
-               .Select(a =>
-               {
-                   Thread.Sleep(10);
-                   return DateTime.Now.Millisecond;
-               });
-            for (int i = 0; i < 10; i++)
-            {
-                foreach (var item in resultX)
-                {
-                    Console.WriteLine(item);
-                }
-            }
+            foreach (var item in result)
+                Console.WriteLine(item);
             Console.ReadKey();
         }
     }
